433801336fe203ac9eb851e441dbca0b376b5d8a
trilokreddy <trilokreddy.m@gmail.com>
2024-07-09 21:39:49 +0530

    Initial Commit

---
+++ SampleTestApp/SampleTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleTestAppApp.swift
+//  SampleTestApp
+//
+//  Created by trilok on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
